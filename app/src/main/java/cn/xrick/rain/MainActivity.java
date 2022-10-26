10ac92812c62020a22cff134f463da02ba0e5cbc
xrick <3068168455qq@gmail.com>
2022-10-26 14:40:19 +0800

    Initial commit

---
+++ app/src/main/java/cn/xrick/rain/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.xrick.rain;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DynamicWeatherView mDynamicWeatherView = (DynamicWeatherView) findViewById(R.id.dynamic_weather_view);
+        mDynamicWeatherView.setType(new RainTypeImpl(this, mDynamicWeatherView));
+    }
+}
